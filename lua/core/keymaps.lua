218a30e4c269f4813632dbfcc022d3a45b6ee3aa
RED9OG <noobcoder56@gmail.com>
2024-02-16 12:31:28 +0530

    first commit

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,13 +1,17 @@
 vim.g.mapleader  = ' '
 vim.api.nvim_set_keymap('i', 'jj', "<Esc>", { noremap = true, silent = true });
 vim.api.nvim_set_keymap('i', 'jk', "<Esc>", { noremap = true, silent = true });
+
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<cr>')
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")vim.wo.number = true
+vim.cmd("set shiftwidth=2")
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-s>', ':w<CR>')
 vim.wo.number = true
+
